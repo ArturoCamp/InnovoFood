724e5d5ebf8aaa07dc740d7333bb8a19b76393bb
ArturoCamp <artcambo20@hotmail.com>
2022-03-26 20:21:34 -0600

    InnovoFood

---
+++ build/precache-manifest.34ac6f227c34bfdf23b28f6b60658f7a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd51ab031ee0102a0a407bcbdd385d45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3313e81dbbde89cb956",
+    "url": "/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "b3313e81dbbde89cb956",
+    "url": "/static/js/2.b20ed1f8.chunk.js"
+  },
+  {
+    "revision": "80fc87440188f061986037f9227eea2a",
+    "url": "/static/js/2.b20ed1f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4eeebc0942e9920dd67",
+    "url": "/static/js/main.3e1ad1ef.chunk.js"
+  },
+  {
+    "revision": "c3069c852c547a185153",
+    "url": "/static/js/runtime-main.403b45e8.js"
+  },
+  {
+    "revision": "5e2b4d5fab296b483867fe0fb4d3ace4",
+    "url": "/static/media/fondo2.5e2b4d5f.jpg"
+  },
+  {
+    "revision": "ee75aad1c0139b4c9ab1729d0679cc7d",
+    "url": "/static/media/fondo4.ee75aad1.png"
+  },
+  {
+    "revision": "7dd18ef68c3aa3d29fb34c56392f34ea",
+    "url": "/static/media/food8.7dd18ef6.jpg"
+  },
+  {
+    "revision": "82ef054d39a3636eea9e5c450934d8f5",
+    "url": "/static/media/food9.82ef054d.jpg"
+  },
+  {
+    "revision": "a045416dac1a61cd3dfbc273b9044ca2",
+    "url": "/static/media/image7.a045416d.jpg"
+  },
+  {
+    "revision": "be27e86c229c8474c7c8693c73fb26ee",
+    "url": "/static/media/image8.be27e86c.jpg"
+  },
+  {
+    "revision": "3bf4d61d5812eeeaab33c4902fc6935d",
+    "url": "/static/media/imagen2.3bf4d61d.jpg"
+  },
+  {
+    "revision": "b1265c3e6f56ce4944170b2dd149f5c7",
+    "url": "/static/media/pizza1.b1265c3e.jpg"
+  }
+]);
